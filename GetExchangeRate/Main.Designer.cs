ded586dc5ad04498f9b8ff20c6b3ecc83be1ffc1
luan.truong <luan.truong@mr.masangroup.com>
2021-02-01 14:26:30 +0700

    Add project files.

---
+++ GetExchangeRate/Main.Designer.cs
@@ -0,0 +1,169 @@
+﻿
+namespace GetExchangeRate
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.ColumnHeader CurrencyName;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.CurrencyCode = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Buy = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Transfer = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Sell = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbDateTime = new System.Windows.Forms.TextBox();
+            this.btRefresh = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            CurrencyName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // CurrencyName
+            // 
+            CurrencyName.Text = "CurrencyName";
+            CurrencyName.Width = 150;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.CurrencyCode,
+            CurrencyName,
+            this.Buy,
+            this.Transfer,
+            this.Sell});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.listView1.GridLines = true;
+            this.listView1.HideSelection = false;
+            this.listView1.HoverSelection = true;
+            this.listView1.Location = new System.Drawing.Point(0, 58);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(706, 392);
+            this.listView1.TabIndex = 3;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // CurrencyCode
+            // 
+            this.CurrencyCode.Text = "CurrencyCode";
+            this.CurrencyCode.Width = 140;
+            // 
+            // Buy
+            // 
+            this.Buy.Text = "Buy";
+            this.Buy.Width = 120;
+            // 
+            // Transfer
+            // 
+            this.Transfer.Text = "Transfer";
+            this.Transfer.Width = 120;
+            // 
+            // Sell
+            // 
+            this.Sell.Text = "Sell";
+            this.Sell.Width = 120;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "DATE TIME:";
+            // 
+            // tbDateTime
+            // 
+            this.tbDateTime.Location = new System.Drawing.Point(116, 19);
+            this.tbDateTime.Name = "tbDateTime";
+            this.tbDateTime.Size = new System.Drawing.Size(163, 20);
+            this.tbDateTime.TabIndex = 0;
+            // 
+            // btRefresh
+            // 
+            this.btRefresh.Image = global::GetExchangeRate.Properties.Resources.refesh;
+            this.btRefresh.Location = new System.Drawing.Point(614, 14);
+            this.btRefresh.Name = "btRefresh";
+            this.btRefresh.Size = new System.Drawing.Size(25, 25);
+            this.btRefresh.TabIndex = 2;
+            this.btRefresh.Tag = "";
+            this.btRefresh.UseVisualStyleBackColor = true;
+            this.btRefresh.Click += new System.EventHandler(this.btRefresh_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btRefresh);
+            this.panel1.Controls.Add(this.tbDateTime);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(706, 52);
+            this.panel1.TabIndex = 4;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 300000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.listView1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "GetVCBExchangeRate";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Main_FormClosing);
+            this.Load += new System.EventHandler(this.Main_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader Buy;
+        private System.Windows.Forms.ColumnHeader Transfer;
+        private System.Windows.Forms.ColumnHeader Sell;
+        private System.Windows.Forms.ColumnHeader CurrencyCode;
+        private System.Windows.Forms.Label label1;
+        protected System.Windows.Forms.TextBox tbDateTime;
+        private System.Windows.Forms.Button btRefresh;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ GetExchangeRate/Main.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Data.SqlClient;
+
+namespace GetExchangeRate
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            timer1.Start();
+            try
+            {
+                XmlDocument xml = new XmlDocument();
+                xml.Load("https://portal.vietcombank.com.vn/Usercontrols/TVPortal.TyGia/pXML.aspx");
+                /*
+                string connectionString;
+                SqlConnection cnn;
+                connectionString = "Data Source=LAPTOP310\\SQLLUANTT;Initial Catalog='';Integrated Security=True";
+                cnn = new SqlConnection(connectionString);
+                cnn.Open();
+                */
+                XmlNodeList Datexml;
+                Datexml = xml.SelectNodes("/ExrateList/DateTime");
+                tbDateTime.Text = Datexml.Item(0).InnerText;
+
+                XmlNodeList noXml;
+                noXml = xml.SelectNodes("/ExrateList/Exrate");
+
+                for (int i = 0; i <= noXml.Count - 1; i++)
+                {
+                    ListViewItem item = new ListViewItem(noXml.Item(i).Attributes["CurrencyCode"].InnerText);
+                    item.SubItems.Add(noXml.Item(i).Attributes["CurrencyName"].InnerText);
+                    item.SubItems.Add(noXml.Item(i).Attributes["Buy"].InnerText);
+                    item.SubItems.Add(noXml.Item(i).Attributes["Transfer"].InnerText);
+                    item.SubItems.Add(noXml.Item(i).Attributes["Sell"].InnerText);
+                    listView1.Items.Add(item);
+                    /*
+                    SqlDataAdapter _sqldataadapter = new SqlDataAdapter("INSERT INTO TEST..VCB_EXCHANGE_RATE(DATETIME, CODE, NAME, BUY, TRANSFER, SELL) values('" + Datexml.Item(0).InnerText + "','" + noXml.Item(i).Attributes["CurrencyCode"].InnerText + "','" + noXml.Item(i).Attributes["CurrencyName"].InnerText + "'," + Double.Parse(noXml.Item(i).Attributes["Buy"].InnerText.Replace("-", "0")) + "," + Double.Parse(noXml.Item(i).Attributes["Transfer"].InnerText.Replace("-", "0")) + "," + Double.Parse(noXml.Item(i).Attributes["Sell"].InnerText.Replace("-", "0")) + ")", cnn);
+                    DataSet ds = new DataSet();
+                    _sqldataadapter.Fill(ds);
+                    */
+
+                }
+                //MessageBox.Show("Data were added to sql server successful!");
+                //cnn.Close();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btRefresh_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            Main_Load(sender, e);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            listView1.Items.Clear();
+            Main_Load(sender, e);
+            this.Refresh();
+        }
+
+        private void Main_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (e.CloseReason != CloseReason.UserClosing)
+                return;
+
+            DialogResult result;
+            result = MessageBox.Show("Are you sure you want to exit?", "Exit Message", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result != DialogResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
